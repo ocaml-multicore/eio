cf7863eff2c0c3efcb9f1b9bd952fd482865fe17
Thomas Leonard <talex5@gmail.com>
2021-04-14 09:09:09 +0100

    Replace ref with record
    This is to allow adding other fields later (e.g. an ID for tracing).

--- lib_promise/promise.ml
+++ lib_promise/promise.ml
@@ -5,42 +5,45 @@ type 'a state =
   | Fulfilled of 'a
   | Broken of exn
 
-type 'a t = 'a state ref
+type 'a t = {
+  mutable state : 'a state;
+}
 
 type 'a u = 'a t
 
 effect Await : 'a waiters -> 'a
 
 let create () =
-  let t = ref (Unresolved (Queue.create ())) in
+  let state = Unresolved (Queue.create ()) in
+  let t = { state } in
   t, t
 
 let await t =
-  match !t with
+  match t.state with
   | Fulfilled x -> x
   | Broken ex -> raise ex
   | Unresolved q ->
     perform (Await q)
 
 let fulfill t v =
-  match !t with
+  match t.state with
   | Broken ex -> invalid_arg ("Can't fulfill already-broken promise: " ^ Printexc.to_string ex)
   | Fulfilled _ -> invalid_arg "Can't fulfill already-fulfilled promise"
   | Unresolved q ->
-    t := Fulfilled v;
+    t.state <- Fulfilled v;
     Queue.iter (fun f -> f (Ok v)) q
 
 let break t ex =
-  match !t with
+  match t.state with
   | Broken orig -> invalid_arg (Printf.sprintf "Can't break already-broken promise: %s -> %s"
                                   (Printexc.to_string orig) (Printexc.to_string ex))
   | Fulfilled _ -> invalid_arg (Printf.sprintf "Can't break already-fulfilled promise (with %s)"
                                   (Printexc.to_string ex))
   | Unresolved q ->
-    t := Broken ex;
+    t.state <- Broken ex;
     Queue.iter (fun f -> f (Error ex)) q
 
-let state t = !t
+let state t = t.state
 
 let add_waiter waiters cb =
   Queue.add cb waiters
