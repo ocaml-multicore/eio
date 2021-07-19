88f0b3107ecd060592b1f00d05d86ada575d9f52
Thomas Leonard <talex5@gmail.com>
2021-07-19 11:48:47 +0100

    Add `Flow.read_methods` for optimised reading
    Allows sources to offer faster alternatives. This is useful for e.g.
    faraday and tls, which allocate their own buffers. The Linux FD sink
    now uses `writev` to send the data directly where possible.

--- lib_eio_linux/eio_linux.ml
+++ lib_eio_linux/eio_linux.ml
@@ -657,6 +657,26 @@ module Objects = struct
     | End_of_file -> ()
     | Unix.Unix_error (Unix.EINVAL, "splice", _) -> fast_copy ?sw src dst
 
+  (* Copy using the [Read_source_buffer] optimisation.
+     Avoids a copy if the source already has the data. *)
+  let copy_with_rsb ?sw rsb dst =
+    try
+      while true do
+        rsb ?sw (writev ?sw dst)
+      done
+    with End_of_file -> ()
+
+  (* Inefficient copying fallback *)
+  let fallback_copy ?sw src dst =
+    with_chunk @@ fun chunk ->
+    let chunk_cs = Uring.Region.to_cstruct chunk in
+    try
+      while true do
+        let got = Eio.Flow.read_into ?sw src chunk_cs in
+        write dst chunk got
+      done
+    with End_of_file -> ()
+
   let flow fd = object (_ : <source; sink; ..>)
     method fd = fd
     method close = FD.close fd
@@ -674,19 +694,18 @@ module Objects = struct
       Cstruct.blit chunk_cs 0 buf 0 got;
       got
 
+    method read_methods = []
+
     method write ?sw src =
       match get_fd_opt src with
       | Some src -> fast_copy_try_splice ?sw src fd
       | None ->
-        (* Inefficient copying fallback *)
-        with_chunk @@ fun chunk ->
-        let chunk_cs = Uring.Region.to_cstruct chunk in
-        try
-          while true do
-            let got = Eio.Flow.read_into ?sw src chunk_cs in
-            write fd chunk got
-          done
-        with End_of_file -> ()
+        let rec aux = function
+          | Eio.Flow.Read_source_buffer rsb :: _ -> copy_with_rsb ?sw rsb fd
+          | _ :: xs -> aux xs
+          | [] -> fallback_copy ?sw src fd
+        in
+        aux( Eio.Flow.read_methods src)
 
     method shutdown cmd =
       Unix.shutdown (FD.get "shutdown" fd) @@ match cmd with
