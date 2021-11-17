021330b258332c9a199833b3da077642be2f945d
Thomas Leonard <talex5@gmail.com>
2021-11-17 13:56:47 +0000

    Show promoted works in benchmark
    Example output:
    
        n_fibers, ns/iter, promoted/iter
            1, 524.20,  0.0016
            2, 394.78,  0.0040
            3, 343.78,  0.0048
            4, 315.77,  0.0072
            5, 300.92,  0.0090
           10, 273.20,  0.0179
           20, 265.16,  0.0357
           30, 261.86,  0.0532
           40, 259.82,  0.0712
           50, 263.72,  0.0887
          100, 371.08, 12.9941
          500, 414.16, 13.0958
         1000, 415.46, 13.1503
        10000, 648.22, 14.0348

--- bench/bench.ml
+++ bench/bench.ml
@@ -3,9 +3,11 @@ open Eio.Std
 let n_fibres = [1; 2; 3; 4; 5; 10; 20; 30; 40; 50; 100; 500; 1000; 10000]
 
 let main ~clock =
-  traceln "n_fibers, ns/iter";
+  Printf.printf "n_fibers, ns/iter, promoted/iter\n%!";
   n_fibres |> List.iter (fun n_fibres ->
       let n_iters = 1000000 / n_fibres in
+      Gc.full_major ();
+      let _minor0, prom0, _major0 = Gc.counters () in
       let t0 = Eio.Time.now clock in
       Switch.run (fun sw ->
           for _ = 1 to n_fibres do
@@ -20,7 +22,9 @@ let main ~clock =
       let time_total = t1 -. t0 in
       let n_total = n_fibres * n_iters in
       let time_per_iter = time_total /. float n_total in
-      traceln "%d, %.2f" n_fibres (1e9 *. time_per_iter)
+      let _minor1, prom1, _major1 = Gc.counters () in
+      let prom = prom1 -. prom0 in
+      Printf.printf "%5d, %.2f, %7.4f\n%!" n_fibres (1e9 *. time_per_iter) (prom /. float n_total)
       (* traceln "%d fibres did %d noops in %.2f seconds : %.2f ns/iter"
            n_fibres n_iters time_total (1e9 *. time_per_iter) *)
     )
