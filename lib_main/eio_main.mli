91a21e71f231de825cc4d0620562e91968bf390f
Thomas Leonard <talex5@gmail.com>
2022-02-08 11:27:36 +0000

    Add eio_main.mli and document the run function

---
+++ lib_main/eio_main.mli
@@ -0,0 +1,22 @@
+(** Select a suitable event loop for Eio. *)
+
+val run : (Eio.Stdenv.t -> unit) -> unit
+(** [run fn] runs an event loop and then calls [fn env] within it.
+
+    [env] provides access to the process's environment (file-system, network, etc).
+
+    When [fn] ends, the event loop finishes.
+
+    This should be called once, at the entry point of an application.
+    It {b must not} be called by libraries.
+    Doing so would force the library to depend on Unix
+    (making it unusable from unikernels or browsers),
+    prevent the user from choosing their own event loop,
+    and prevent using the library with other Eio libraries.
+
+    [run] will select an appropriate event loop for the current platform.
+    On many systems, it will use {!Eio_luv.run}.
+
+    On recent-enough versions of Linux, it will use {!Eio_linux.run}.
+    You can override this by setting the $EIO_BACKEND environment variable to
+    either "io-uring" or "luv". *)
