80f2f5c218f600123db529ce03b2ff93f86d45e2
Thomas Leonard <talex5@gmail.com>
2022-06-24 10:47:07 +0100

    Split core modules to eio_core
    This makes it more obvious that these are independent of the OS
    abstractions, ensures that the OS abstractions only use exposed APIs,
    and allows moving other interfaces into separate files.

--- lib_eio/eio.ml
+++ lib_eio/eio.ml
@@ -1,41 +1,4 @@
-module Cancel = Cancel
-
-module Private = struct
-  module Fiber_context = Cancel.Fiber_context
-
-  module Effects = struct
-    type 'a enqueue = 'a Suspend.enqueue
-    type _ Effect.t += 
-      | Suspend = Suspend.Suspend
-      | Fork = Fiber.Fork
-      | Get_context = Cancel.Get_context
-      | Trace : (?__POS__:(string * int * int * int) -> ('a, Format.formatter, unit, unit) format4 -> 'a) Effect.t
-  end
-
-  module Effect = Effect
-  module Ctf = Ctf
-
-  let traceln_mutex = Mutex.create ()
-
-  let default_traceln ?__POS__:pos fmt =
-    let k go =
-      let b = Buffer.create 512 in
-      let f = Format.formatter_of_buffer b in
-      go f;
-      Option.iter (fun (file, lnum, _, _) -> Format.fprintf f " [%s:%d]" file lnum) pos;
-      Format.pp_close_box f ();
-      Format.pp_print_flush f ();
-      let msg = Buffer.contents b in
-      Ctf.label msg;
-      let lines = String.split_on_char '\n' msg in
-      Mutex.lock traceln_mutex;
-      Fun.protect ~finally:(fun () -> Mutex.unlock traceln_mutex) @@ fun () ->
-      List.iter (Printf.eprintf "+%s\n") lines;
-      flush stderr
-    in
-    Format.kdprintf k ("@[" ^^ fmt)
-
-end
+include Eio__core
 
 let traceln ?__POS__ fmt =
   try
@@ -43,10 +6,7 @@ let traceln ?__POS__ fmt =
   with Unhandled ->
     Private.default_traceln ?__POS__ fmt
 
-module Promise = Promise
-module Fiber = Fiber
 module Fibre = Fiber
-module Switch = Switch
 
 module Std = struct
   module Promise = Promise
