b49e7088e6d40f779c0f7a55e6e9ee561dd71a0e
Thomas Leonard <talex5@gmail.com>
2022-09-20 09:58:13 +0100

    Add Eio.Time.Timeout
    This may be useful for passing timeouts as arguments.

--- lib_eio/eio.mli
+++ lib_eio/eio.mli
@@ -124,14 +124,39 @@ module Time : sig
   val sleep : #clock -> float -> unit
   (** [sleep t d] waits for [d] seconds. *)
 
-  val with_timeout : #clock -> float -> (unit -> ('a, 'e) result) -> ('a, [> `Timeout] as 'e) result
-  (** [with_timeout clock d fn] runs [fn ()] but cancels it after [d] seconds. *)
+  (** {2 Timeouts} *)
 
   exception Timeout
 
+  val with_timeout : #clock -> float -> (unit -> ('a, 'e) result) -> ('a, [> `Timeout] as 'e) result
+  (** [with_timeout clock d fn] runs [fn ()] but cancels it after [d] seconds. *)
+
   val with_timeout_exn : #clock -> float -> (unit -> 'a) -> 'a
   (** [with_timeout_exn clock d fn] runs [fn ()] but cancels it after [d] seconds,
-      raising exception [Timeout]. *)
+      raising exception {!exception-Timeout}. *)
+
+  (** Timeout values. *)
+  module Timeout : sig
+    type t
+
+    val of_s : #clock -> float -> t
+    (** [of_s clock duration] is a timeout of [duration] seconds, as measured by [clock].
+        Internally, this is just the tuple [(clock, duration)]. *)
+
+    val none : t
+    (** [none] is an infinite timeout. *)
+
+    val run : t -> (unit -> ('a, 'e) result) -> ('a, [> `Timeout] as 'e) result
+    (** [run t fn] runs [fn ()] but cancels it if it takes longer than allowed by timeout [t]. *)
+
+    val run_exn : t -> (unit -> 'a) -> 'a
+    (** [run_exn t fn] runs [fn ()] but cancels it if it takes longer than allowed by timeout [t],
+        raising exception {!exception-Timeout}. *)
+
+    val pp : t Fmt.t
+    (** [pp] formats a timeout as a duration (e.g. "5s").
+        This is intended for use in error messages and logging and is rounded. *)
+  end
 end
 
 (** Operations on open files. *)
--- lib_eio/time.ml
+++ lib_eio/time.ml
@@ -13,3 +13,34 @@ let sleep t d = sleep_until t (now t +. d)
 
 let with_timeout t d = Fiber.first (fun () -> sleep t d; Error `Timeout)
 let with_timeout_exn t d = Fiber.first (fun () -> sleep t d; raise Timeout)
+
+module Timeout = struct
+  type t =
+    | Timeout of clock * float
+    | Unlimited
+
+  let none = Unlimited
+  let of_s clock time = Timeout ((clock :> clock), time)
+
+  let run t fn =
+    match t with
+    | Unlimited -> fn ()
+    | Timeout (clock, d) ->
+      Fiber.first (fun () -> sleep clock d; Error `Timeout) fn
+
+  let run_exn t fn =
+    match t with
+    | Unlimited -> fn ()
+    | Timeout (clock, d) ->
+      Fiber.first (fun () -> sleep clock d; raise Timeout) fn
+
+  let pp f = function
+    | Unlimited -> Fmt.string f "(no timeout)"
+    | Timeout (_clock, d) ->
+      if d >= 0.001 && d < 0.1 then
+        Fmt.pf f "%.2gms" (d *. 1000.)
+      else if d < 120. then
+        Fmt.pf f "%.2gs" d
+      else
+        Fmt.pf f "%.2gm" (d /. 60.)
+end
