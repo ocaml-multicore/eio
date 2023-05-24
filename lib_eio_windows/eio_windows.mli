f959c72504bc74fbfb976efab45e4341861df36d
Thomas Leonard <talex5@gmail.com>
2023-05-24 11:24:42 +0100

    eio_windows: use Eio_unix.Stdenv.base type
    Otherwise, it can't be used with `Eio_main.run`.
    
    Also, get CI to test an example using `Eio_main`.

--- lib_eio_windows/eio_windows.mli
+++ lib_eio_windows/eio_windows.mli
@@ -1,19 +1,7 @@
 (** Fallback Eio backend for Windows using OCaml's [Unix.select]. *)
 
-type stdenv = <
-  stdin  : <Eio.Flow.source; Eio_unix.Resource.t>;
-  stdout : <Eio.Flow.sink; Eio_unix.Resource.t>;
-  stderr : <Eio.Flow.sink; Eio_unix.Resource.t>;
-  net : Eio.Net.t;
-  domain_mgr : Eio.Domain_manager.t;
-  clock : Eio.Time.clock;
-  mono_clock : Eio.Time.Mono.t;
-  fs : Eio.Fs.dir Eio.Path.t;
-  cwd : Eio.Fs.dir Eio.Path.t;
-  secure_random : Eio.Flow.source;
-  debug : Eio.Debug.t;
->
-(** An extended version of {!Eio.Stdenv.t} with some extra features available on Windows. *)
+type stdenv = Eio_unix.Stdenv.base
+(** An extended version of {!Eio.Stdenv.base} with some extra features available on Windows. *)
 
 val run : (stdenv -> 'a) -> 'a
 (** [run main] runs an event loop and calls [main stdenv] inside it.
