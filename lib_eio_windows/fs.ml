a0e5da2d68175f2eaf8d958e9296718d0ed75def
Daniel Ntege <itsupport@bugemauniv.ac.ug>
2024-10-31 12:34:58 +0300

    trying to remove some bugs from the tests
    Co-Authored-By: Patrick Ferris <patrick@sirref.org>

--- lib_eio_windows/fs.ml
+++ lib_eio_windows/fs.ml
@@ -181,9 +181,7 @@ end = struct
     with_parent_dir t path @@ fun dirfd path ->
     Err.run (Low_level.symlink ~link_to dirfd) path
 
-  let chmod t ~follow ~perm path =
-    failwith "TODO: Implement Windows chmod functionality"
-  
+
   let close t = t.closed <- true
 
   let open_dir t ~sw path =
