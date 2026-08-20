7cc9ccfeec86f80a09d3a6713ecd982bfabeca35
Patrick Ferris <patrick@sirref.org>
2026-08-20 09:55:35 +0100

    Test get_all
    Fix off-by-one error in splitting environment variables.

--- lib_eio/unix/vars.ml
+++ lib_eio/unix/vars.ml
@@ -4,7 +4,7 @@ let split_in_two c s =
   try
     let first_occurence = String.index s c in
     let first = String.sub s 0 first_occurence in
-    let second = String.sub s (first_occurence + 1) (String.length s - first_occurence) in
+    let second = String.sub s (first_occurence + 1) (String.length s - first_occurence - 1) in
     Some (first, second)
   with Not_found -> None
 
